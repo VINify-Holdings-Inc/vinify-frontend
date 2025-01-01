92f055a8bcc3b102e67af4725b662d8d45f2b6d0
amit9792 <amit.chauhan@techwagger.com>
2025-01-01 16:47:06 +0530

    new

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -33,7 +33,7 @@ export class ContactComponent {
                   message: ['', [Validators.required, ]],
                   email: ['', [Validators.required,strictEmailValidator() ]], 
                   phone: ['', [Validators.required, Validators.pattern(/^[0-9]{10}$/)]],
-                 // phone: ['', [Validators.required, Validators.pattern(/^\+?[1-9]\d{1,14}$/) ]],
+                 /// phone: ['', [Validators.required, Validators.pattern(/^\+?[1-9]\d{1,14}$/) ]],
                 //  phone: ['', [Validators.required, Validators.pattern('^(\\+1\\s?)?\\(?\\d{3}\\)?[-\\s]?\\d{3}[-\\s]?\\d{4}$') ]],
                 });
               }
