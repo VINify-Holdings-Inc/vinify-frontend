a393793113d3fd214d5349330a334f0c72b7d136
sadanand900 <sadanand@hashtaglabs.in>
2025-01-07 11:33:58 +0530

    latest

--- src/app/components/layout/title-details-main/title-tab/title-tab.component.ts
+++ src/app/components/layout/title-details-main/title-tab/title-tab.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import {  Component, AfterViewInit, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-title-tab',
@@ -11,6 +13,39 @@ export class TitleTabComponent {
     filerIcon: string = 'assets/images/icons/filter-lines.svg';
     calendarIcon: string = 'assets/images/icons/calendar.svg';
     pdfIcon: string = 'assets/images/icons/pdf.svg';
+
+
+    
+      searchValue :string="";
+        
+        @Input() tableData :any[]=[];
+        @Input() page :number=0;
+        @Input() totalPages :number=0;
+      
+        @Output() handelPaginagtion = new EventEmitter <any>();
+        @Output() handelSearch = new EventEmitter <any>();
+       searchHideShow :boolean =false;
+      
+      onClick(pages:any){
+         this.handelPaginagtion.emit(pages);
+      } 
+      
+       
+      getSearchVal(){
+        if(this.searchValue==""){
+          this.searchHideShow = !this.searchHideShow;
+        }else{
+          this.handelSearch.emit(this.searchValue);
+        }
+        
+        
+      }
+      onType(value: string){
+        if(value==""){
+          this.handelSearch.emit(value);
+          //this.searchHideShow = !this.searchHideShow;
+        }
+      }
 
 
 
