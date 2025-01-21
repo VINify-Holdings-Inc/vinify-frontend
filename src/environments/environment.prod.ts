81edd73d3c7dac43f3dc4aea576af8ddd64f12fd
amit9792 <amit.chauhan@techwagger.com>
2025-01-21 14:19:49 +0530

    new

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,7 @@
 export const environment = {
    api_url: 'https://mvmapi.techwagger.com/api',
     img_url: 'https://mvmapi.techwagger.com/api/uploads',
-    production: true,
-    
+    production: true, 
   };
 
 
