ab33da52b47ca7d3858a7537360ca98a7c830232
sadanand900 <sadanand@hashtaglabs.in>
2025-01-21 20:53:37 +0530

    latest

--- src/app/components/layout/dashboard-header/dashboard-header.component.ts
+++ src/app/components/layout/dashboard-header/dashboard-header.component.ts
@@ -82,7 +82,9 @@ export class DashboardHeaderComponent implements OnInit , OnDestroy {
  }
 
  getSearchVal(){
-  if(this.searchValue!=""){
+  if(this.searchValue && this.searchValue.trim() !== ""){
+    
+  //  console.log("test space",this.searchValue);
     this.getVinSearch(this.searchValue.trim());
       /* const timestamp = new Date().getTime(); 
         this.router.navigate(['/title-details'], { queryParams: { vin: this.searchValue, refresh: timestamp }}).then(() => {
