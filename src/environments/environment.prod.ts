d8f3aa78b8c44283637246a48156bd0490faadf3
amit9792 <amit.chauhan@techwagger.com>
2025-01-21 16:10:55 +0530

    new

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-     api_url: 'http://localhost:4800/api',
-   img_url: 'http://localhost:4800/api/uploads',
+   api_url: 'https://mvmapi.techwagger.com/api',
+    img_url: 'https://mvmapi.techwagger.com/api/uploads',
     production: true,
     
   };
