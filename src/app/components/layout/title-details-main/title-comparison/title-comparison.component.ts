93dc951b03866443b6296e55151d8e81248019cc
amit638824 <amit.chauhan@techwagger.com>
2025-01-22 17:45:03 +0530

    new

---
+++ src/app/components/layout/title-details-main/title-comparison/title-comparison.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-title-comparison',
+  imports: [],
+  templateUrl: './title-comparison.component.html',
+  styleUrl: './title-comparison.component.css'
+})
+export class TitleComparisonComponent {
+
+}
