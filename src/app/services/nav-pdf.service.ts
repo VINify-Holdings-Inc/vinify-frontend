3b2dec85ca79deaf3326b2a1ba2ed4724fdc4d3b
sadanand900 <sadanand@hashtaglabs.in>
2025-03-11 14:40:15 +0530

    report fix

--- src/app/services/nav-pdf.service.ts
+++ src/app/services/nav-pdf.service.ts
@@ -14,7 +14,7 @@ export class NavPdfService {
 
   private capitalizePipe = new CapitalizePipe();
   constructor(private dateFormate: DateFormatPipe,) { }
-
+  
   generatePDF(
     companyName: string,
     vin: string,
@@ -26,8 +26,9 @@ export class NavPdfService {
     logImage:any,
     fileName: string = 'Vehicle_History_Report.pdf',
   ): void {
-    const doc = new jsPDF();
 
+    const doc = new jsPDF();
+    const nmvtlogo = 'assets/images/nmvtis-1.png';
     // Header Section (Every Page)
     const addHeader = () => {
       // Add Logo
@@ -44,7 +45,7 @@ export class NavPdfService {
       doc.setFontSize(10);
       doc.setFont('helvetica', 'normal');
       doc.text(`VIN: ${vin}`, 80, 30);
-      doc.text('2008 NISSAN Sentra', 140, 30);
+     // doc.text('2008 NISSAN Sentra', 140, 30);
 
         // Horizontal Line
         doc.setDrawColor(0, 0, 0);
@@ -56,9 +57,9 @@ export class NavPdfService {
       let y = 45; // Position below the header
     
       // Vehicle Title
-      doc.setFontSize(18);
-      doc.setFont('helvetica', 'bold');
-      doc.text('2005 NISSAN Sentra SAMPLE REPORT', 15, y);
+      // doc.setFontSize(18);
+      // doc.setFont('helvetica', 'bold');
+      // doc.text('2005 NISSAN Sentra SAMPLE REPORT', 15, y);
       y += 10;
     
       // Warning Tagline
@@ -169,6 +170,7 @@ export class NavPdfService {
     doc.setFillColor(255, 0, 0);
     doc.rect(15, y, 180, 10, 'F');
     doc.setTextColor(255, 255, 255);
+    doc.setFontSize(10);
     doc.text('Warning - At least one negative title event has been reported.', 20, y + 7);
     doc.setTextColor(0, 0, 0);
     y += 15;
@@ -181,14 +183,14 @@ export class NavPdfService {
       item.brand ? item.brand.split(' - ')[0] : "-",
       item.state || "-",
     ]);
-    
+    doc.setTextColor(0, 0, 0);
     // Draw Title Information Table
     (doc as any).autoTable({
       startY: y += 5, 
       theme: 'grid',
       head: [tableColumn],
       body: tableRows,
-      headStyles: { fillColor: [207, 75, 95], fontSize: 8 },
+      headStyles: { fillColor: [237, 237, 237], fontSize: 8,fontColor:'black' },
       bodyStyles: { fontSize: 7 },
       margin: { top: 37 , bottom: 25},
       didDrawPage: (data: any) => {
@@ -207,7 +209,8 @@ export class NavPdfService {
       doc.setFillColor(255, 0, 0);
       doc.rect(15, y, 180, 10, 'F');
       doc.setTextColor(255, 255, 255);
-      doc.text('Warning - Brand total loss events have been reported.', 20, y + 7);
+      doc.setFontSize(10);
+      doc.text('Warning – at least one negative title or cautionary DMV title brands have been reported to VINData History.', 20, y + 7);
       doc.setTextColor(0, 0, 0);
       y += 15;
   
@@ -225,7 +228,7 @@ const brandRows = brandData.map((item) => [
   theme: 'grid',
   head: [brandColumns],
   body: brandRows,
-  headStyles: { fillColor: [207, 75, 95], fontSize: 8 },
+  headStyles: { fillColor: [237, 237, 237], fontSize: 8 },
   bodyStyles: { fontSize: 7 },
   margin: { top: 37,bottom: 25 },
   didDrawPage: (data: any) => {
@@ -245,7 +248,8 @@ y = (doc as any).lastAutoTable.finalY + 10;
     doc.setFillColor(255, 0, 0);
     doc.rect(15, y, 180, 10, 'F');
     doc.setTextColor(255, 255, 255);
-    doc.text('Warning - Junk, salvage or insurance total loss events have been reported.', 20, y + 7);
+    doc.setFontSize(10);
+    doc.text('Warning - junk, salvage or insurance total loss events have been reported to VINData History.', 20, y + 7);
     doc.setTextColor(0, 0, 0);
     y += 15;
 
@@ -263,7 +267,7 @@ y = (doc as any).lastAutoTable.finalY + 10;
       theme: 'grid',
       head: [jsiColumns],
       body: jsiRows,
-      headStyles: { fillColor: [207, 75, 95], fontSize: 8 },
+      headStyles: { fillColor: [237, 237, 237], fontSize: 8 },
       bodyStyles: { fontSize: 7 },
       margin: { top: 37,bottom: 25 },
       didDrawPage: (data: any) => {
@@ -308,6 +312,7 @@ y = (doc as any).lastAutoTable.finalY + 10;
         // doc.text(disclaimerLines[i], 10, yPosition); // Add line
         doc.text(disclaimerLines[i], 14, yPosition, { align: 'left' });
         yPosition += lineHeight; // Increment yPosition for the next line
+        y=yPosition;
       }
 
 
@@ -321,13 +326,20 @@ y = (doc as any).lastAutoTable.finalY + 10;
 
   // Description
   doc.setFontSize(10);
-  const description = `Your VINData History report checks for and reports information from the following high-quality data sources. 
-  Please see our report sections page, FAQ, terms of service, and disclaimer for more information.`;
+  const description = `Your VIN ALARM report checks for and reports information from the following high-quality data sources.
+  Please see our report sections page, FAQ, terms of service and disclaimer for more information.`;
   doc.text(doc.splitTextToSize(description, 180), 15, y);
   y += 15;
 
+  const sources1=[{
+   
+    description: `The National Motor Vehicle Title Information System (NMVTIS) is an electronic system that provides consumers with valuable information about a vehicle's condition and history. 
+    Prior to purchasing a vehicle, NMVTIS allows consumers, dealers, wholesale auctions, vehicle lenders and insurers to find information on the vehicle's title, most recent odometer reading, brand history, and, in some cases, historical theft data.`,
+     
+  }]
+
   // Loop through data sources
-  sources.forEach((source, index) => {
+  sources1.forEach((source, index) => {
     // Check for page break
     if (y + 50 > doc.internal.pageSize.height - 20) {
       doc.addPage();
@@ -335,14 +347,14 @@ y = (doc as any).lastAutoTable.finalY + 10;
     }
 
     // Add Logo
-    doc.addImage(logImage, 'PNG', 15, y, 40, 20);
+    doc.addImage( nmvtlogo, 'PNG', 15, y, 40, 20);
     
     // Add Text
     doc.text(doc.splitTextToSize(source.description, 140), 60, y + 5);
     
     // Add Copyright
-    doc.setFontSize(8);
-    doc.text(doc.splitTextToSize(source.copyright, 180), 15, y + 30);
+    // doc.setFontSize(8);
+    // doc.text(doc.splitTextToSize(source.copyright, 180), 15, y + 30);
     y += 45; // Move Y down for the next block
   });
 
